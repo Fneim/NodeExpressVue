b321d26830c30c364917ffca2521d81b616e6809
fneim <neimfargin@gmx.com.tr>
2018-11-23 21:37:07 -0800

    auth option changes on button click

---
+++ public/index.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: "#home",
+  data: {
+    seen: true,
+    button: {
+      txt: "Login"
+    }
+  },
+  methods: {
+    changeButtonTxt: function() {
+      if(this.seen !== true) {
+        this.button.txt = "Sign Up";
+      } else {
+        this.button.txt = "Login"
+      }
+    }
+  }
+});
--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const path = require("path");
 const PORT = process.env.port || 8080;
 const app = express();
 
-app.use(express.static("/index.js"));
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
